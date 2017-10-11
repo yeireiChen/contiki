a12e638ce7c5d6550c665e6ffb1822af926045cd
yoyo890121 <yoyo890121@hotmail.com>
2017-10-11 18:21:28 +0800

    fix SensorData nodeTime

--- tools/collect-view/src/org/contikios/contiki/collect/SensorData.java
+++ tools/collect-view/src/org/contikios/contiki/collect/SensorData.java
@@ -55,7 +55,11 @@ public class SensorData implements SensorInfo {
   public SensorData(Node node, int[] values, long systemTime) {
     this.node = node;
     this.values = values;
-    this.nodeTime = ((values[TIMESTAMP1] << 16) + values[TIMESTAMP2]) * 1000L;
+    long nodeTime = ((values[TIMESTAMP1] << 16) + values[TIMESTAMP2]) * 1000L;
+    while (nodeTime > 131072000) {
+      nodeTime -= 131072000;
+    }
+    this.nodeTime = nodeTime;
     this.systemTime = systemTime;
     this.seqno = values[SEQNO];
   }
